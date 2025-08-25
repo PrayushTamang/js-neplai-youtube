fadb743d493f5ef7b4e7c6e6151d60fef49b0cdb
PrayushTamang <prayushtamang5@gmail.com>
2025-08-25 07:36:50 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33 easily convert
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);                             => true output in terminal
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);                             => false output in terminal
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);                             => false output in terminal
+
+
+// let isLoggedIn = "Prayush"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);                                => true output in terminal
+
+// 1 =>true 0 =>false
+// "" =>false
+// "Prayush" =>true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
